0a8f3c550865c89d032a97a273fefd75a87868aa
sumiem <msumiec@gmail.com>
2021-03-27 21:22:09 +0100

    Added 3 functions checking sudoku conditions for a cell

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # SudokuSolver
-Code wiil find a solution to given sudoku table if possible.
+
+Sudoku is a logic-based, combinatorial number-placement puzzle. In classic sudoku, the objective is to fill a 9×9 grid with digits so that each column, each row, and each of the nine 3×3 subgrids that compose the grid (also called "boxes", "blocks", or "regions") contain all of the digits from 1 to 9. The puzzle setter provides a partially completed grid, which for a well-posed puzzle has a single solution. [source: https://en.wikipedia.org/wiki/Sudoku]
+
--- main.py
+++ main.py
@@ -1,21 +1,51 @@
 """coding: utf-8
 Name: SudokuSolver
-Description: An algorithm for solving given sudoku board if there exist a solution.
+Description: An algorithm for solving given sudoku board if there exist a solution. Will use backtracking algorithm.
 Input: numpy.array
 Output: numpy.array
-"""
-import numpy as np
 
-sudoku = np.array([[9, 0, 0, 0, 8, 0, 3, 0, 0],
-                   [0, 0, 0, 2, 5, 0, 0, 0, 0],
-                   [0, 2, 0, 3, 0, 0, 0, 0, 4],
-                   [0, 9, 4, 0, 0, 0, 0, 0, 0],
-                   [0, 0, 0, 7, 3, 0, 5, 6, 0],
-                   [7, 0, 5, 0, 6, 0, 4, 0, 0],
-                   [0, 0, 7, 8, 0, 3, 9, 0, 0],
-                   [0, 0, 1, 0, 0, 0, 0, 0, 3],
-                   [3, 0, 0, 0, 0, 0, 0, 0, 2]])
+Remarks:
+Each cell can have position (i, j) from (0, 0) to (8, 8)
+Each board has 9 * 9 = 81 cells so I need to create conditions for each cell based on it's position.
+There are 3 conditions each for row, column, and square.
+The biggest problem is square condition as it changes based on position of the cell.
+i.e. There are 9 squares inside a sudoku board and they also have their position from (0, 0) to (2, 2)
+cell (0, 2) is in square (0, 0) but (0, 3) is in square (0, 1)
+I should probably create a class Cell with Position, Number, Status attributes
+Status can be a property of a cell
+"""
+import itertools
+# example from wikipedia
+sudoku = [[5, 3, 0, 0, 7, 0, 0, 0, 0],
+          [6, 0, 0, 1, 9, 5, 0, 0, 0],
+          [0, 9, 8, 0, 0, 0, 0, 6, 0],
+          [8, 0, 0, 0, 6, 0, 0, 0, 3],
+          [4, 0, 0, 8, 0, 3, 0, 0, 1],
+          [7, 0, 0, 0, 2, 0, 0, 0, 6],
+          [0, 6, 0, 0, 0, 0, 2, 8, 0],
+          [0, 0, 0, 4, 1, 9, 0, 0, 5],
+          [0, 0, 0, 0, 8, 0, 0, 7, 9]]
+
+
+def solved(area: list, is_square=False) -> bool:
+    if is_square:
+        #  making square flat because it's a nested list
+        area = list(itertools.chain.from_iterable(area))
+    return len(area) == len(set(area))
+
+
+def check_row(table: list, cell_position: tuple) -> bool:
+    return solved(table[cell_position[0]][:])
+
+
+def check_col(table: list, cell_position: tuple) -> bool:
+    return solved([i[cell_position[1]] for i in table])
+
+
+def check_square(table: list, cell_position: tuple):
+    square = [i[((cell_position[1] // 3) * 3):(3 + (cell_position[1] // 3) * 3)] for i in table[((cell_position[0] // 3) * 3):(3 + (cell_position[0] // 3) * 3)]]
+    return solved(square, True)
 
 
 if __name__ == "__main__":
-    pass
+    print(check_square(sudoku, (0, 1)))
